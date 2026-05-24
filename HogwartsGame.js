0019d28bf85d94aaf8964a092d2a12b54a7b941b
FR0d1k <srasscazov2006@gmail.com>
2026-05-25 02:14:04 +0300

    Rebuild with updated main.cpp

--- HogwartsGame.js
+++ HogwartsGame.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Laptop\AppData\Local\Temp\tmphb4t74sa.js
+// include: C:\Users\Laptop\AppData\Local\Temp\tmpv1xu_t6l.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -204,21 +204,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: C:\Users\Laptop\AppData\Local\Temp\tmphb4t74sa.js
-// include: C:\Users\Laptop\AppData\Local\Temp\tmpa2qsksxb.js
+// end include: C:\Users\Laptop\AppData\Local\Temp\tmpv1xu_t6l.js
+// include: C:\Users\Laptop\AppData\Local\Temp\tmpycy7kzo8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Laptop\AppData\Local\Temp\tmpa2qsksxb.js
-// include: C:\Users\Laptop\AppData\Local\Temp\tmpv_651j1e.js
+  // end include: C:\Users\Laptop\AppData\Local\Temp\tmpycy7kzo8.js
+// include: C:\Users\Laptop\AppData\Local\Temp\tmpaf7uycr5.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Laptop\AppData\Local\Temp\tmpv_651j1e.js
+  // end include: C:\Users\Laptop\AppData\Local\Temp\tmpaf7uycr5.js
 
 
 var arguments_ = [];
